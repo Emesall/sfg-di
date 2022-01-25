b8e2feaa51de54f9dcec1362c93733ae55f8b922
Emesall <mati1230@gmail.com>
2022-01-25 13:47:12 +0100

    Primary profiles config

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -2,13 +2,24 @@ package guru.springframework.sfgdi.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.I18nEnglishGreetingService;
+import guru.springframework.sfgdi.services.PrimaryGreetingService;
 import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
 
 @Configuration
 public class GreetingServiceConfig {
 
+	@Profile("EN")
+	@Bean("i18nService")
+	I18nEnglishGreetingService i18nEnglishGreetingService() {
+		return new I18nEnglishGreetingService();
+	}
+	
+	
 	@Bean
 	ConstructorGreetingService constructorGreetingService() {
 		return new ConstructorGreetingService();
@@ -18,4 +29,10 @@ public class GreetingServiceConfig {
 	PropertyInjectedGreetingService propertyInjectedGreetingService() {
 		return new PropertyInjectedGreetingService();
 	}
+	@Primary
+	@Bean
+	PrimaryGreetingService primaryGreetingService() {
+		return new PrimaryGreetingService();
+	}
+	
 }
--- src/main/java/guru/springframework/sfgdi/services/PropertyInjectedGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/PropertyInjectedGreetingService.java
@@ -9,4 +9,6 @@ public class PropertyInjectedGreetingService implements GreetingService {
     public String sayGreeting() {
         return "Hello World - Property";
     }
+    
+
 }
